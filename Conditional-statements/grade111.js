629369704a8b6a9ca7fd6668f7edad9704b799bf
dako-23 <iordan.megaparts@gmail.com>
2024-01-10 23:56:10 +0200

    10.01

---
+++ Conditional-statements/grade111.js
@@ -0,0 +1,17 @@
+function grade111 (input){
+
+let grade = Number (input[0]);
+
+if (grade > 2.00 && grade <= 2.49) {
+    console.log("Poor!");
+} else if (grade > 2.49 && grade <= 3.49) {
+    console.log("Average!");
+} else if (grade > 3.49 && grade <= 4.49) {
+    console.log("Good!");
+} else if (grade > 4.49 && grade <= 5.49) {
+    console.log("Very Good");
+} else if (grade > 5.49) {
+    console.log("Excellent!");
+}
+}
+grade111 ([3.49]);

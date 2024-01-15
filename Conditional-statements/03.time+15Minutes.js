83d93b5d2cc5502db232a2eea6f99b467c38d424
dako-23 <iordan.megaparts@gmail.com>
2024-01-15 09:45:41 +0200

    15.01

---
+++ Conditional-statements/03.time+15Minutes.js
@@ -0,0 +1,23 @@
+function time15min (input){
+
+let hours = Number(input[0]);
+let minutes = Number(input[1]);
+let minutesPlus = minutes + 15
+
+if (minutesPlus >= 60) {
+        hours = hours + 1
+        minutesPlus = minutesPlus - 60
+}
+if (hours > 23) {
+        hours = 0
+}
+
+if (minutesPlus < 10) {
+        console.log(`${hours}:0${minutesPlus}`)
+    } else {
+        console.log(`${hours}:${minutesPlus}`)
+}
+
+
+} 
+time15min(["1", "46"]);

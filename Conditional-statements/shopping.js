3ef31e329001a44fea9ee38d17d9705683b32900
dako-23 <iordan.megaparts@gmail.com>
2024-01-15 21:29:49 +0200

    15.01

--- Conditional-statements/shopping.js
+++ Conditional-statements/shopping.js
@@ -7,13 +7,23 @@ let ramCount = Number(input[3]);
 
 let gpuSum = gpuCount * 250;
 let cpuSum = gpuSum * 0.35;
-let ramSum = gpuSum * 0.10 * 3;
-let totalSum = gpuSum + cpuSum + ramSum;
+let ramSum = gpuSum * 0.10;
+let finalPriceCpu = cpuCount * cpuSum;
+let finalPriceRam = ramCount * ramSum;
+let totalSum = finalPriceCpu + finalPriceRam + gpuSum;
 
+if (gpuCount > cpuCount){
+   let discount = totalSum * 0.15;
+   totalSum = totalSum - discount; 
+}
 
+if (totalSum <= budget){
+    console.log(`You have ${(budget - totalSum).toFixed(2)} leva left!`);
+} else {
+    console.log(`Not enough money! You need ${(totalSum - budget).toFixed(2)} leva more!`);
+}
 
-
-    
 
 }
+//shopping (["920.45","3","1","1"])
 shopping (["900","2","1","3"])

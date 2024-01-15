5b0d64262c1c4fe2cbd048d652b7b995c2eee99d
dako-23 <iordan.megaparts@gmail.com>
2024-01-15 19:57:00 +0200

    15.01

---
+++ Conditional-statements/shopping.js
@@ -0,0 +1,19 @@
+function shopping (input){
+
+let budget = Number(input[0]);
+let gpuCount = Number(input[1]);
+let cpuCount = Number(input[2]);
+let ramCount = Number(input[3]);
+
+let gpuSum = gpuCount * 250;
+let cpuSum = gpuSum * 0.35;
+let ramSum = gpuSum * 0.10 * 3;
+let totalSum = gpuSum + cpuSum + ramSum;
+
+
+
+
+    
+
+}
+shopping (["900","2","1","3"])

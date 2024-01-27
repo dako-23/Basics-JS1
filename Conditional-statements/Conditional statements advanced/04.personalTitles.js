642e09e7ed49745486f61cbf09e7ea9c753a787e
dako-23 <iordan.megaparts@gmail.com>
2024-01-27 18:45:21 +0200

    27.01

---
+++ Conditional-statements/Conditional statements advanced/04.personalTitles.js
@@ -0,0 +1,26 @@
+function personalTitles(input) {
+
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    if (gender === "f") {
+        if (age >= 16) {
+            console.log("Ms.");
+        }
+        else {
+            console.log("Miss");
+        }
+    }
+    if (gender === "m") {
+        if (age >= 16) {
+            console.log("Mr.");
+        }
+        else {
+            console.log("Master");
+        }
+    }
+
+
+
+}
+personalTitles(["12", "f"])

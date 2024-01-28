bac04ebeb1a4fef35d0ffbb916141c4d3979c77c
dako-23 <iordan.megaparts@gmail.com>
2024-01-28 17:57:53 +0200

    28.01

---
+++ Conditional statements advanced/04.fishingBoat.js
@@ -0,0 +1,42 @@
+function fishingBoat(input) {
+
+    let budget = Number(input[0]);
+    let season = input[1];
+    let fisherCount = Number(input[2]);
+
+    let price = 0;
+
+    switch (season) {
+        case "Spring":
+            price = 3000; break;
+        case "Summer":
+        case "Autumn":
+            price = 4200; break;
+        case "Winter":
+            price = 2600; break;
+    }
+    if (fisherCount <= 6) {
+        price *= 0.9;
+    } else if (fisherCount >= 7 && fisherCount <= 11) {
+        price *= 0.85;
+    } else {
+        price *= 0.75;
+    }
+
+    if (fisherCount % 2 === 0 && season !== "Autumn") {
+        price *= 0.95;
+    }
+
+    let moneyLeft = budget - price;
+    let moneyNeeded = price - budget;
+    
+    if(budget >= price){
+        console.log(`Yes! You have ${moneyLeft.toFixed(2)} leva left.`);
+    } else{
+        console.log(`Not enough money! You need ${moneyNeeded.toFixed(2)} leva.`);
+    }
+
+}
+fishingBoat(["2000","Winter","13"])
+//fishingBoat(["3600","Autumn","6"])
+//fishingBoat(["3000", "Summer", "11"])

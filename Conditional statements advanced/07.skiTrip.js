97b65d4fa83a0765e878eaaee2c637603f4f94c2
dako-23 <iordan.megaparts@gmail.com>
2024-01-28 18:29:44 +0200

    28.01

---
+++ Conditional statements advanced/07.skiTrip.js
@@ -0,0 +1,44 @@
+function skiTrip(input) {
+
+    let dayCount = Number(input[0]);
+    let nightCount = dayCount - 1;
+    let spaseType = input[1];
+    let feedback = input[2];
+
+    let price = 0;
+
+    switch (spaseType) {
+        case "room for one person":
+            price = nightCount * 18; break;
+        case "apartment":
+            price = nightCount * 25;
+            if (nightCount < 10) {
+                price *= 0.7;
+            } else if (nightCount <= 15) {
+                price *= 0.65;
+            } else {
+                price *= 0.5;
+            }
+            break;
+        case "president apartment":
+            price = nightCount * 35;
+            if (nightCount < 10) {
+                price *= 0.9;
+            } else if (nightCount <= 15) {
+                price *= 0.85;
+            } else {
+                price *= 0.8;
+            }
+            break;
+    }
+
+    if (feedback === "positive") {
+        price *= 1.25;
+    } else if (feedback === "negative") {
+        price *= 0.9;
+    }
+    console.log(price.toFixed(2));
+
+
+}
+skiTrip(["14", "apartment", "positive"])

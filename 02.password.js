5844c49918c83d19babec212f3ccf77fe7055d8d
dako-23 <iordan.megaparts@gmail.com>
2024-02-12 14:39:39 +0200

    12

--- 02.password.js
+++ 02.password.js
@@ -1,7 +1,16 @@
-function password(input){
+function password(input) {
 
+    let userName = input[0];
+    let password = input[1];
+    let index = 2;
+    let currentWord = input[index];
 
+    while (currentWord !== password) {
+        index++;
+        currentWord = input[index]
 
+    }
+    console.log(`Welcome ${userName}!`);
 
 
 
@@ -9,4 +18,8 @@ function password(input){
 
 
 }
-password
+password(["Nakov",
+    "1234",
+    "Pass",
+    "1324",
+    "1234"])

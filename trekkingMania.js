4938990341ee02f456473cd664e4be6dfb2ef386
dako-23 <iordan.megaparts@gmail.com>
2024-02-12 11:30:48 +0200

    12

--- trekkingMania.js
+++ trekkingMania.js
@@ -2,25 +2,43 @@ function trekkingMania(input) {
 
     let groupCount = Number(input[0])
 
+    let group1 = 0;
+    let group2 = 0;
+    let group3 = 0;
+    let group4 = 0;
+    let group5 = 0;
 
+    let totalCount = 0;
 
     for (let i = 1; i < groupCount + 1; i++) {
-        let personCount = input[i];
 
+        let personCount = Number(input[i]);
+        totalCount += personCount;
 
-
-
-        
+        if (personCount < 6) {
+            group1 += personCount
+        } else if (personCount < 13) {
+            group2 += personCount
+        } else if (personCount < 26) {
+            group3 += personCount
+        } else if (personCount < 41) {
+            group4 += personCount
+        } else {
+            group5 += personCount
+        }
     }
 
+    let percent1 = (group1 / totalCount) * 100;
+    let percent2 = (group2 / totalCount) * 100;
+    let percent3 = (group3 / totalCount) * 100;
+    let percent4 = (group4 / totalCount) * 100;
+    let percent5 = (group5 / totalCount) * 100;
 
-
-
-
-
-
-
-
+    console.log(percent1.toFixed(2));
+    console.log(percent2.toFixed(2));
+    console.log(percent3.toFixed(2));
+    console.log(percent4.toFixed(2));
+    console.log(percent5.toFixed(2));
 
 
 
